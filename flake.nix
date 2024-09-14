594b93557017f36aed35e90612509187a23019d7
Fabian Hauser <fabian@fh2.ch>
2024-09-14 18:18:15 +0300

    Initial version

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    deploy-rs.url = "github:serokell/deploy-rs";
+    tabi = {
+      url = "github:welpo/tabi/main";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs-unstable,
+      deploy-rs,
+      tabi,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      domain = "fabianhauser.ch";
+      version = "2022";
+      pkgs = import nixpkgs-unstable { inherit system; };
+      deployPkgs = import nixpkgs-unstable {
+        inherit system;
+        overlays = [
+          deploy-rs.overlay
+          (self: super: {
+            deploy-rs = {
+              inherit (pkgs) deploy-rs;
+              lib = super.deploy-rs.lib;
+            };
+          })
+        ];
+      };
+      preparePhase = ''
+        rm -rf themes/tabi
+        ln -s ${tabi} themes/tabi
+      '';
+
+    in
+    {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+      checks.${system}.default = pkgs.stdenv.mkDerivation {
+        inherit version;
+        name = "${domain}-${version}";
+        buildInputs = [ pkgs.zola ];
+        src = ./src;
+        installPhase = ''
+          set -euo pipefail
+          ${preparePhase}
+          zola --root . check
+          mkdir $out
+        '';
+      };
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        inherit version;
+        name = "${domain}-${version}";
+        buildInputs = [ pkgs.zola ];
+        src = ./src;
+        installPhase = ''
+          ${preparePhase}
+          zola --root . build --output-dir $out
+        '';
+      };
+
+      deploy.nodes.lindberg-webapps = {
+        hostname = "lindberg-webapps.backplane.net.qo.is";
+        profiles.${domain} = {
+          sshUser = "nginx-${domain}";
+          path = deployPkgs.deploy-rs.lib.activate.noop self.packages.${system}.default;
+          profilePath = "/var/lib/nginx-${domain}/root";
+        };
+      };
+
+      apps.${system} = {
+        default =
+          let
+            zola = pkgs.writeShellScriptBin "zola" ''
+              cd src
+              ${preparePhase}
+              ${pkgs.zola}/bin/zola --root . ''${@}
+            '';
+          in
+          {
+            type = "app";
+            program = "${zola}/bin/zola";
+          };
+        deploy = {
+          type = "app";
+          program = "${pkgs.deploy-rs}/bin/deploy";
+        };
+      };
+    };
+}
---
+++ src/content/_index.md
@@ -0,0 +1,24 @@
++++
+#path = "/"
+title = "About"
+template = "home.html"
+
+[extra]
+header = {title = "Hi! I'm Fabian.", img = "img/profile.webp" }
++++
+
+I’m a passionate traveller, mountaineer, [photographer](https://portfolio.pixelfed.social/fabianhauser) and lifelong learner.
+Currently, I'm taking a sabbatical to travel eastern Asia 🌏
+
+Professionally, I've been active in computer science and teaching with a focus on software engineering, distributed systems and [open source software](./projects).
+
+Before, I was bringing secure and privacy first communication to the general public at [Threema](https://threema.ch/)
+and giving lectures on Software Engineering at [OST – Fachhochschule Ostschweiz](https://www.ost.ch/).
+
+&nbsp;
+
+[📬 fabian@fh2.ch](mailto:fabian@fh2.ch)  
+[📷 Pixelfed Portfolio](https://portfolio.pixelfed.social/fabianhauser)  
+[⚙️  github.com/fabianhauser](https://github.com/fabianhauser)  
+🔒 **GPG-Key**: [`0x8a52a140bebf7d2c`](https://keys.openpgp.org/search?q=fabian%40fh2.ch)  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fingerprint: `50B7 11F4 3DFD 2018 DCE6 E8D0 8A52 A140 BEBF 7D2C`
